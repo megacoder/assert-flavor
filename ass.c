529228aba3db9c4340a80ffd6423d0fd9b47f074
tf135c <tf135c>
2006-08-16 16:57:33 +0000

    Descript the purpose of this test.

--- ass.c
+++ ass.c
@@ -3,16 +3,7 @@
  */
 
 #include <stdio.h>
-
-#define	offsetof(t,e) (&((t *) NULL)->e)
-
-typedef struct	{
-	int	i;
-	float	f;
-	double	d;
-	char	c[32];
-	short	s;
-} OO_t;
+#include <assert.h>
 
 int
 main(
@@ -20,12 +11,12 @@ main(
 	char * *	argv
 )
 {
-	static char const	fmt[] = { "Offset of '%s' is %u.\n" };
-
-	printf( fmt, "i", offsetof( OO_t, i ) );
-	printf( fmt, "f", offsetof( OO_t, f ) );
-	printf( fmt, "d", offsetof( OO_t, d ) );
-	printf( fmt, "c", offsetof( OO_t, c ) );
-	printf( fmt, "s", offsetof( OO_t, s ) );
+	/*
+	 * Some implementations of assert(3) keep the exact () contents
+	 * and use that, including any commentary, as the message.  Some
+	 * have already elided the comments.  See what this compiler does.
+	 */
+	assert( 1 == 1 /* This should be fine */ );
+	assert( 0 == 1 /* This should produce a runtime error */ );
 	return( 0 );
 }
